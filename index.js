7ad05aa912c6910ddbf0824a7b50bada916af921
lovephimu <soe.nlb@gmail.com>
2023-05-01 17:53:09 +0200

    add readme

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 // LIBRARIES VARIABLES INPUT
+
+import fs from 'node:fs';
 import { Configuration, OpenAIApi } from 'openai';
 import { createInterface } from 'readline';
 import apiAiKey from './apikey.js';
 
-const apiKey = apiAiKey;
-const aiUrl = 'https://monkeylearn.com/sentiment-analysis-online/';
+const apiKey = apiAiKey || undefined;
 const userInput = process.argv.slice(2).join(' ');
 console.log(userInput);
 const missionStatementOne = 'Analyze the following input for sentiment value: ';
 const missionStatementTwo =
   ' - first tell me if this text is more positive, negative or neutral. Secondly list the emotions and their percentage. Each list item should include the corresponding name of the emotions and an emoji that can be read in vs code terminal.';
 
+const colorStart = `\x1b[38;2;0;255;127m`;
+const colorEnd = `\x1b[0m`;
 // FUNCTIONS
 
 // User Input functions
@@ -35,7 +38,9 @@ const readLineAsync = (msg) => {
 
 const startApp = async () => {
   const userRes = await readLineAsync(
-    'What would you like me to run through a sentiment analysis? \n',
+    colorStart +
+      'What would you like me to run through a sentiment analysis? \n' +
+      colorEnd,
   );
   readline.close();
   console.log(
@@ -50,7 +55,9 @@ const startApp = async () => {
 
 // Welcome Message + Input
 
-console.log(`
+console.log(
+  colorStart +
+    `
 ##############################
 ##############################
 ##############################
@@ -60,24 +67,40 @@ console.log(`
 ##############################
 ##############################
 ##############################
-`);
+` +
+    colorEnd,
+);
 
-console.log(`
-Hello, please input a word or text through command line! (Alternatively upload a text file by typing UPLOAD)
+if (!apiKey) {
+  console.log('API Key not defined - please contact developer');
+}
+
+console.log(
+  colorStart +
+    `
+Hello, please input a word or text through command line! (Alternatively use a sample text file by typing SAMPLE)` +
+    colorEnd +
+    `
 Also if you are reviewing this: please note that this isn't working online since I cannot upload my openAi API key.
-Just ask me and I'll show you the functionality\n`);
+Just ask me and I'll show you the functionality\n`,
+);
 
-const msgToAi = await startApp();
+let msgToAi = await startApp();
 
-if (msgToAi === 'UPLOAD') {
-  console.log('Sorry still under construction');
-  process.exit(0);
+if (msgToAi === 'SAMPLE') {
+  fs.readFile('./text.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      process.exit(0);
+    }
+    msgToAi = data;
+  });
 }
 
 // openAi communication
 
 const configuration = new Configuration({
-  apiKey: apiAiKey,
+  apiKey: apiKey,
 });
 const openai = new OpenAIApi(configuration);
 const response = await openai.createCompletion({
---
+++ readme.md
@@ -0,0 +1,3 @@
+# THIS WILL ONLY WORK LOCALLY!!!
+
+Unfortunately you cannot hardcode an API key into the code thus the code will throw an error because it's trying to access a file which is listed on .gitignore
