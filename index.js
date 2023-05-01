5109c29fc6376dd2ce2c02450b5f42082a7a1780
lovephimu <soe.nlb@gmail.com>
2023-05-01 13:54:13 +0200

    add user interface input

--- index.js
+++ index.js
@@ -1,16 +1,58 @@
+// LIBRARIES VARIABLES INPUT
+import { createInterface } from 'readline';
+
 const aiUrl = 'https://monkeylearn.com/sentiment-analysis-online/';
+const aiApiKey = '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';
 const userInput = process.argv.slice(2).join(' ');
 console.log(userInput);
 
-const formData = {
-  textarea: userInput,
-};
+// FUNCTIONS
 
-const response = await fetch(aiUrl, {
-  method: 'POST',
-  body: formData,
+// User Input functions
+
+const readline = createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-const responseText = await response.text();
+const readLineAsync = (msg) => {
+  return new Promise((resolve) => {
+    readline.question(msg, (userRes) => {
+      resolve(userRes);
+    });
+  });
+};
 
-console.log(responseText);
+const startApp = async () => {
+  const userRes = await readLineAsync(
+    'What would you like me to run through a sentiment analysis? \n',
+  );
+  readline.close();
+  console.log(
+    'Your response was: ' +
+      userRes +
+      ' — Thanks! Please wait a moment for the AI to analyze it',
+  );
+  return userRes;
+};
+
+// PROGRAM ON RUNTIME
+
+// Welcome Message + Input
+
+console.log(`
+##############################
+##############################
+##############################
+#####                    #####
+##### SENTIMENT-ANALYSIS #####
+#####                    #####
+##############################
+##############################
+##############################
+`);
+
+console.log(`Hello, please input a word or text through command line! \n`);
+
+const msgToAi = await startApp();
+console.log(msgToAi);
